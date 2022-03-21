a755d3783039bd804bcda823918860bdb7d6c92d
Likhon Sheikh <93394527+LikhonSheikhBot@users.noreply.github.com>
2022-03-22 03:23:10 +0600

    Add files via upload

---
+++ _next/static/eMrmHcfQDjZqw7ixWgn_K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84d8ce4b2a6027a7abdc.js"],"/_error":["static/chunks/pages/_error-c8c86af7ea5c719e7504.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
